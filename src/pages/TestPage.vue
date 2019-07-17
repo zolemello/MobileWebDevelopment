b24714d4a54fe14926e9827a9299161038ee7c00
zolemello <mello.soledad@gmail.com>
2019-07-17 20:24:50 -0300

    subiendo por primera vez

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,56 @@
+
+
+<template>
+
+	      
+    <div class="row" id="topNav"> 
+        
+      <div class="col-sm-4"> <a href="login.html">Log In</a> </div>
+      <div class="col-sm-4">  <img id="logo" src="D:\Full Stack\Modulo 3\NYSL Web_MV\img/nysl_logo.png" height="100px" width="100px"> </div>
+      <div class="col-sm-4">  <a href="contact.html">Contact</a></div>
+        
+           
+     <div id="bodycontent">
+       <div id="pageTittle">
+     <h1>Upcoming Events</h1>
+       </div>
+
+      <div id="upcoming">
+        <h2>August 4</h2>
+        <p class="indent">NYSL Fundraiser</p>
+
+        <h2>August 16</h2>
+        <p class="indent">Season Kick-off: Meet the Teams</p>
+        
+        <h2>September 1</h2>
+        <p class="indent"> First Game of the Season (Check Game Schedule for details)</p>
+      </div>
+    </div>
+
+
+    <div id="bottomNav"> 
+      
+      <a href="index.html">Home</a>
+      <a href="calendar.html">Calendar</a>
+      <a href="info.html">Game Information</a>
+      <a href="extras.html">Extras</a>
+          </div>  
+    
+
+
+   
+  </div>
+
+
+
+</template>
+
+<script> 
+export default {
+name:"TestPage"
+
+}
+</script>
+
+<style scoped>
+</style>
