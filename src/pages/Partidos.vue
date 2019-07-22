57f41504dbfe429f60e5a4dd321dbbe530686ef5
zolemello <mello.soledad@gmai.com>
2019-07-22 18:14:59 -0300

    actualizando pagina partidos

--- src/pages/Partidos.vue
+++ src/pages/Partidos.vue
@@ -2,20 +2,88 @@
 <template>
 
   <div>
+
     <ul class="sinPuntos">
-      <li v-for="partido in partidos">
+      <li v-for="partido in partidos" v-bind:key="partido.id">
         <partido-del-dia v-bind="partido"/>
       </li>
     </ul>
+    
+
+   <div class="row">
+
+      <div class="col-xl-3 col-md-6">
+          <stats-card>
+            <div slot="header" class="icon-warning">
+              <i class="nc-icon nc-chart text-warning"></i>
+            </div>
+            <div slot="content">
+              <p class="card-category">Capacity</p>
+              <h4 class="card-title">105GB</h4>
+            </div>
+            <div slot="footer">
+              <i class="fa fa-refresh"></i>Updated now
+            </div>
+          </stats-card>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6">
+          <stats-card>
+            <div slot="header" class="icon-success">
+              <i class="nc-icon nc-light-3 text-success"></i>
+            </div>
+            <div slot="content">
+              <p class="card-category">Revenue</p>
+              <h4 class="card-title">$1,345</h4>
+            </div>
+            <div slot="footer">
+              <i class="fa fa-calendar-o"></i>Last day
+            </div>
+          </stats-card>
+    </div>
+
+    <div class="col-xl-3 col-md-6">
+          <stats-card>
+            <div slot="header" class="icon-danger">
+              <i class="nc-icon nc-vector text-danger"></i>
+            </div>
+            <div slot="content">
+              <p class="card-category">Errors</p>
+              <h4 class="card-title">23</h4>
+            </div>
+            <div slot="footer">
+              <i class="fa fa-clock-o"></i>Last day
+            </div>
+          </stats-card>
+    </div>
+
+    <div class="col-xl-3 col-md-6">
+          <stats-card>
+            <div slot="header" class="icon-info">
+              <i class="nc-icon nc-favourite-28 text-primary"></i>
+            </div>
+            <div slot="content">
+              <p class="card-category">Followers</p>
+              <h4 class="card-title">+45</h4>
+            </div>
+            <div slot="footer">
+              <i class="fa fa-refresh"></i>Updated now
+            </div>
+          </stats-card>
+    </div>
 
   </div>
 
+  </div>
 
-  
 </template>
 
 <script>
 import PartidoDelDia from '../components/Partidos/PartidoDelDia'
+import Card from '../components/Cards/Card'
+import ChartCard from '../components/Cards/ChartCard'
+import StatsCard from '../components/Cards/StatsCard'
 import { scrypt } from 'crypto';
 	export default {
     data() {
